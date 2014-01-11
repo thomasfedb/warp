ac7365f000ae74a59d13f25e207c7a3ab8950ae8
Thomas Drake-Brockman <thom@sfedb.com>
2014-01-11 03:01:40 +0100

    Prevent problem with Rails 4.1 builds.

--- spec/support/model_helpers.rb
+++ spec/support/model_helpers.rb
@@ -2,6 +2,14 @@ module ModelHelpers
   def build_model(&blk)
     let(:model) do
       Class.new(ActiveRecord::Base) do
+        def self.name
+          "TestModel"
+        end
+
+        def self.primary_key
+          "id"
+        end
+
         def self.columns
           @columns ||= []
         end
