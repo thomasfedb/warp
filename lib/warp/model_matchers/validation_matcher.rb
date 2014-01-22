2bcd2a2adaf5a87564e3a71bee388673df895c68
Thomas Drake-Brockman <thom@sfedb.com>
2014-01-22 17:29:40 +0100

    Updated validation matcher to matcher ActiveModel validators also.

--- lib/warp/model_matchers/validation_matcher.rb
+++ lib/warp/model_matchers/validation_matcher.rb
@@ -1,42 +1,41 @@
 module Warp
   module ModelMatchers
     class ValidationMatcher < Warp::ModelMatchers::Matcher
-      VALIDATORS = [
-        ActiveModel::Validations::AcceptanceValidator,
-        ActiveModel::Validations::ConfirmationValidator,
-        ActiveModel::Validations::ExclusionValidator,
-        ActiveModel::Validations::FormatValidator,
-        ActiveModel::Validations::InclusionValidator,
-        ActiveModel::Validations::LengthValidator,
-        ActiveModel::Validations::NumericalityValidator,
-        ActiveModel::Validations::PresenceValidator
-      ]
+      VALIDATORS = {
+        accptance: [ActiveModel::Validations::AcceptanceValidator],
+        confirmation: [ActiveModel::Validations::ConfirmationValidator],
+        exclusion: [ActiveModel::Validations::ExclusionValidator],
+        format: [ActiveModel::Validations::FormatValidator],
+        inclusion: [ActiveModel::Validations::InclusionValidator],
+        length: [ActiveModel::Validations::LengthValidator],
+        numericality: [ActiveModel::Validations::NumericalityValidator],
+        presence: [ActiveModel::Validations::PresenceValidator]
+      }
 
       # AbsenceValidator added in Rails 4
       if defined?(ActiveModel::Validations::AbsenceValidator)
-        VALIDATORS << ActiveModel::Validations::AbsenceValidator
+        VALIDATORS[:absence] = [ActiveModel::Validations::AbsenceValidator]
       end
 
       if defined?(ActiveRecord)
-        VALIDATORS.concat [
-          ActiveRecord::Validations::AssociatedValidator,
-          ActiveRecord::Validations::UniquenessValidator
-        ]
+        VALIDATORS.merge!({
+          associated: [ActiveRecord::Validations::AssociatedValidator],
+          uniqueness: [ActiveRecord::Validations::UniquenessValidator]
+        })
       end
 
       # In Rails 4 PresenceValidator is subclassed by AR
       if defined?(ActiveRecord::Validations::PresenceValidator)
-        VALIDATORS.delete(ActiveModel::Validations::PresenceValidator)
-        VALIDATORS.push(ActiveRecord::Validations::PresenceValidator)
+        VALIDATORS[:presence] << ActiveRecord::Validations::PresenceValidator
       end
 
       VALIDATORS.freeze
 
-      attr_reader :attr_name, :validator_class, :validator_options, :display_options
+      attr_reader :attr_name, :validator_classes, :validator_options, :display_options
 
-      def initialize(attr_name, validator_class, validator_options, display_options)
+      def initialize(attr_name, validator_classes, validator_options, display_options)
         @attr_name = attr_name
-        @validator_class = validator_class
+        @validator_classes = validator_classes
         @validator_options = validator_options
         @display_options = display_options
       end
@@ -45,7 +44,7 @@ module Warp
         @model_or_instance = model_or_instance
 
         validators.any? do |validator|
-          if values_match?(validator_class, validator.class)
+          if validator_classes.any? {|validator_class| values_match?(validator_class, validator.class) }
             validator_options.nil? || values_match?(validator_options, validator.options)
           else
             false
@@ -62,7 +61,7 @@ module Warp
       end
 
       def description
-        str = "have validator #{validator_class.name} on :#{attr_name}"
+        str = "have validator #{validator_classes.map(&:name).join(", or ")} on :#{attr_name}"
         str << " with options #{description_of(display_options)}" if display_options.present?
         str
       end
@@ -74,15 +73,14 @@ module Warp
       end
     end
 
-    ValidationMatcher::VALIDATORS.each do |validator|
-      validator_name = validator.name.match(/::(?<name>\w+)Validator/)[:name].underscore.to_sym
-      method_name = (validator_name == :associated) ? "validate_associated" : "validate_#{validator_name}_of"
+    ValidationMatcher::VALIDATORS.each do |validator, validator_classes|
+      method_name = (validator == :associated) ? "validate_associated" : "validate_#{validator}_of"
 
       define_method method_name do |attr_name, *options|
         display_options = options.first
         options = display_options.try(:dup) || {}
 
-        case validator_name 
+        case validator
         when :uniqueness
           options = {case_sensitive: true}.merge(options)
         when :acceptance
@@ -91,7 +89,7 @@ module Warp
 
         options = nil if options.empty?
 
-        ValidationMatcher.new(attr_name, validator, options, display_options)
+        ValidationMatcher.new(attr_name, validator_classes, options, display_options)
       end
     end
   end
