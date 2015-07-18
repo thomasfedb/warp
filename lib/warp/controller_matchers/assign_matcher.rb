f26547cf73dd4bd9d2054db656b28804bc9d4ffb
Thomas Drake-Brockman <thom@sfedb.com>
2015-07-18 15:09:40 +0800

    Updated specs for assign matchers and refactored.

--- lib/warp/controller_matchers/assign_matcher.rb
+++ lib/warp/controller_matchers/assign_matcher.rb
@@ -2,92 +2,171 @@ require "warp/matcher"
 
 module Warp
   module ControllerMatchers
-    class AssignMatcher < Warp::Matcher
-      attr_reader :assign_key, :assign_with, :assign_with_a, :assign_with_a_new
-      attr_reader :controller, :failure_message, :failure_message_when_negated, :description
+    class AssignMatcherBuilder
+      attr_reader :assign_key, :controller
 
       def initialize(assign_key, controller)
         @assign_key = assign_key
         @controller = controller
       end
 
-      def with(assign_eq)
-        @assign_with = assign_eq
-        self
+      def with(value)
+        AssignWithMatcher.new(@controller, @assign_key, value)
       end
 
-      def with_a(assign_class)
-        @assign_with_a = assign_class
-        self
+      def with_a(klass)
+        AssignWithAMatcher.new(@controller, @assign_key, klass)
       end
 
-      def with_a_new(assign_class)
-        @assign_with_a_new = assign_class
-        self
+      def with_a_new(klass)
+        AssignWithANewMatcher.new(@controller, @assign_key, klass)
       end
-      
+
+      def method_missing(method, *args)
+        matcher.send(method, *args)
+      end
+
+      private
+
+      def matcher
+        @matcher ||= AssignMatcher.new(@controller, @assign_key)
+      end
+    end
+
+    def assign(assign_key)
+      AssignMatcherBuilder.new(controller, assign_key)
+    end
+
+    class AssignMatcher < Warp::Matcher
+      attr_reader :assign_key, :controller
+
+      def initialize(assign_key, controller)
+        @assign_key = assign_key
+        @controller = controller
+      end
+
       def matches?(actual)
         @controller = actual if actual.is_a?(ActionController::Metal)
-
-        if multiple_assertions?
-          raise "Only one of .with, .with_a, and .with_a_new can be used with the assigns matcher."
-        end
-
-        return check_assign_with if assign_with
-        return check_assign_with_a if assign_with_a
-        return check_assign_with_a_new if assign_with_a_new
-        return check_assign
+        check_assign
       end
-      
+
+      def description
+        "assign @#{assign_key}"
+      end
+
+      def failure_message
+        "expected @#{assign_key} to be assigned"
+      end
+
+      def failure_message_when_negated
+        "expected @#{assign_key} to not be assigned"
+      end
+
       private
 
       def check_assign
-        @description = "assign @#{assign_key}"
-        @failure_message = "expected @#{assign_key} to be assigned"
-        @failure_message_when_negated = "expected @#{assign_key} to not be assigned"
-
         values_match?(false, assign_value.nil?)
-      end
-
-      def check_assign_with
-        @description = "assign @#{assign_key} with #{description_of(assign_with)}"
-        @failure_message = "expected @#{assign_key} to be assigned with #{description_of(assign_with)} but was assigned with #{assign_value.inspect}"
-        @failure_message_when_negated = "expected @#{assign_key} to not be assigned with #{description_of(assign_with)}"
-
-        values_match?(assign_with, assign_value)
-      end
-
-      def check_assign_with_a
-        @description = "assign @#{assign_key} with an instance of #{assign_with_a.name}"
-        @failure_message = "expected @#{assign_key} to be assigned with an instance of #{assign_with_a.name} but was assigned with an instance of #{assign_value.class.name}"
-        @failure_message_when_negated = "expected @#{assign_key} to not be assigned with an instance of #{assign_with_a.name}"
-
-        has_ancestor?(assign_with_a, assign_value)
-      end
-
-      def check_assign_with_a_new
-        @description = "assign @#{assign_key} with a new instance of #{assign_with_a_new.name}"
-        @failure_message = "expected @#{assign_key} to be assigned with a new instance of #{assign_with_a_new.name} but was assigned with a #{assign_value.persisted? ? "persisted" : "new"} instance of #{assign_value.class.name}"
-        @failure_message_when_negated = "expected @#{assign_key} to not be assigned with a new instance of #{assign_with_a_new.name}"
-
-        has_ancestor?(assign_with_a_new, assign_value) && values_match?(false, assign_value.persisted?)
       end
 
       def assign_value
         controller.view_assigns[assign_key.to_s]
       end
-
-      def multiple_assertions?
-        [@assign_with, @assign_with_a, @assign_with_a_new].compact.size > 1
-      end
-
       def has_ancestor?(expected_class, actual)
         actual.class.ancestors.any? {|ancestor| values_match?(expected_class, ancestor) }
       end
     end
 
-    def assign(assign_key)
-      AssignMatcher.new(assign_key, controller)
+    class AssignWithMatcher < AssignMatcher
+      attr_reader :assign_with
+
+      def initialize(controller, assign_key, assign_with)
+        super(controller, assign_key)
+        @assign_with = assign_with
+      end
+
+      def description
+        "assign @#{assign_key} with #{description_of(assign_with)}"
+      end
+
+      def failure_message
+        if assign_value.nil?
+          "expected @#{assign_key} to be assigned with #{description_of(assign_with)} but was not assigned"
+        else
+          "expected @#{assign_key} to be assigned with #{description_of(assign_with)} but was assigned with #{assign_value.inspect}"
+        end
+      end
+
+      def failure_message_when_negated
+        "expected @#{assign_key} to not be assigned with #{description_of(assign_with)}"
+      end
+
+      private
+
+      def check_assign
+        values_match?(assign_with, assign_value)
+      end
+    end
+
+    class AssignWithAMatcher < AssignMatcher
+      attr_reader :assign_with_a
+
+      def initialize(controller, assign_key, assign_with_a)
+        super(controller, assign_key)
+        @assign_with_a = assign_with_a
+      end
+
+      def description
+        "assign @#{assign_key} with an instance of #{assign_with_a.name}"
+      end
+
+      def failure_message
+        if assign_value.nil?
+          "expected @#{assign_key} to be assigned with an instance of #{assign_with_a.name} but was not assigned"
+        else
+          "expected @#{assign_key} to be assigned with an instance of #{assign_with_a.name} but was assigned with an instance of #{assign_value.class.name}"
+        end
+      end
+
+      def failure_message_when_negated
+        "expected @#{assign_key} to not be assigned with an instance of #{assign_with_a.name}"
+      end
+
+      private
+
+      def check_assign
+        has_ancestor?(assign_with_a, assign_value)
+      end
+    end
+
+    class AssignWithANewMatcher < AssignMatcher
+      attr_reader :assign_with_a_new
+
+      def initialize(controller, assign_key, assign_with_a_new)
+        super(controller, assign_key)
+        @assign_with_a_new = assign_with_a_new
+      end
+
+      def description
+        "assign @#{assign_key} with a new instance of #{assign_with_a_new.name}"
+      end
+
+      def failure_message
+        if assign_value.nil?
+          "expected @#{assign_key} to be assigned with a new instance of #{assign_with_a_new.name} but was not assigned"
+        else
+          "expected @#{assign_key} to be assigned with a new instance of #{assign_with_a_new.name} but was assigned with a #{assign_value.try(:persisted?) ? "persisted" : "new"} instance of #{assign_value.class.name}"
+        end
+      end
+
+      def failure_message_when_negated
+        "expected @#{assign_key} to not be assigned with a new instance of #{assign_with_a_new.name}"
+      end
+
+      private
+
+      def check_assign
+        has_ancestor?(assign_with_a_new, assign_value) && values_match?(false, assign_value.try(:persisted?))
+      end
     end
   end
 end
--- spec/warp/controller_matchers/assign_matcher_spec.rb
+++ spec/warp/controller_matchers/assign_matcher_spec.rb
@@ -4,12 +4,12 @@ describe Warp::ControllerMatchers::AssignMatcher, type: :controller do
   with_contexts do
     context "with implicit controller" do
       build_controller(:controller)
-      
+
       let(:_controller) { controller }
 
       subject { matcher.tap {|m| m.matches?(Object.new) } }
     end
-    
+
     context "with explicit controller" do
       build_controller(:other_controller)
       let(:controller) { double("fake controller") }
@@ -18,7 +18,7 @@ describe Warp::ControllerMatchers::AssignMatcher, type: :controller do
 
       subject { matcher.tap {|m| m.matches?(other_controller) } }
     end
-    
+
     behaviour do
       let(:matcher) { assign(:assign) }
 
@@ -34,7 +34,7 @@ describe Warp::ControllerMatchers::AssignMatcher, type: :controller do
         end
 
         specify { expect(subject).to match(_controller) }
-      
+
         describe_failure_message_when_negated do
           specify { expect(subject).to eq "expected @assign to not be assigned" }
         end
@@ -46,18 +46,13 @@ describe Warp::ControllerMatchers::AssignMatcher, type: :controller do
         end
 
         specify { expect(subject).to_not match(_controller) }
-        
+
         describe_failure_message do
           specify { expect(subject).to eq "expected @assign to be assigned" }
         end
       end
 
       describe "#with" do
-        controller_action do
-          @assign = spec.actual_assign_value
-        end
-
-        let(:actual_assign_value) { "foobar" }
         let(:expected_assign_value) { "foobar" }
 
         let(:matcher) { super().with(expected_assign_value) }
@@ -68,32 +63,47 @@ describe Warp::ControllerMatchers::AssignMatcher, type: :controller do
           specify { expect(subject).to eq "assign @assign with #{expected_assign_value.inspect}" }
         end
 
-        context "with the right value" do
-          specify { expect(subject).to match(_controller) }
-        
-          describe_failure_message_when_negated do
-            specify { expect(subject).to eq "expected @assign to not be assigned with #{expected_assign_value.inspect}" }
+        context "when assigned" do
+          controller_action do
+            @assign = spec.actual_assign_value
+          end
+
+          context "with the right value" do
+            let(:actual_assign_value) { expected_assign_value }
+
+            specify { expect(subject).to match(_controller) }
+
+            describe_failure_message_when_negated do
+              specify { expect(subject).to eq "expected @assign to not be assigned with #{expected_assign_value.inspect}" }
+            end
+          end
+
+          context "with the wrong value" do
+            let(:actual_assign_value) { "foobaz" }
+
+            specify { expect(subject).to_not match(_controller) }
+
+            describe_failure_message do
+              specify { expect(subject).to eq "expected @assign to be assigned with #{expected_assign_value.inspect} but was assigned with #{actual_assign_value.inspect}" }
+            end
           end
         end
 
-        context "with the wrong value" do
-          let(:expected_assign_value) { "foobaz" }
+        context "when not assigned" do
+          controller_action do
+            # no assign
+          end
 
           specify { expect(subject).to_not match(_controller) }
-          
+
           describe_failure_message do
-            specify { expect(subject).to eq "expected @assign to be assigned with #{expected_assign_value.inspect} but was assigned with #{actual_assign_value.inspect}" }
+            specify { expect(subject).to eq "expected @assign to be assigned with #{expected_assign_value.inspect} but was not assigned" }
           end
         end
       end
 
       describe "#with_a" do
-        controller_action do
-          @assign = spec.actual_assign_class.new
-        end
-
-        let(:actual_assign_class) { Class.new.tap {|klass| allow(klass).to receive(:name) { "FooClass" } } }
-        let(:expected_assign_class) { actual_assign_class }
+        let(:expected_assign_class) { Class.new.tap {|klass| allow(klass).to receive(:name) { "FooClass" } } }
 
         let(:matcher) { super().with_a(expected_assign_class) }
 
@@ -103,40 +113,48 @@ describe Warp::ControllerMatchers::AssignMatcher, type: :controller do
           specify { expect(subject).to eq "assign @assign with an instance of #{expected_assign_class.name}" }
         end
 
-        context "with the right class" do
-          specify { expect(subject).to match(_controller) }
-            
-          describe_failure_message_when_negated do
-            specify { expect(subject).to eq "expected @assign to not be assigned with an instance of #{expected_assign_class.name}" }
+        context "when assigned" do
+          controller_action do
+            @assign = spec.actual_assign_class.new
+          end
+
+          context "with the right class" do
+            let(:actual_assign_class) { expected_assign_class }
+
+            specify { expect(subject).to match(_controller) }
+
+            describe_failure_message_when_negated do
+              specify { expect(subject).to eq "expected @assign to not be assigned with an instance of #{expected_assign_class.name}" }
+            end
+          end
+
+          context "with the wrong class" do
+            let(:actual_assign_class) { Class.new.tap {|klass| allow(klass).to receive(:name) { "BarClass" } } }
+
+            specify { expect(subject).to_not match(_controller) }
+
+            describe_failure_message do
+              specify { expect(subject).to eq "expected @assign to be assigned with an instance of #{expected_assign_class.name} but was assigned with an instance of #{actual_assign_class.name}"
+       }
+            end
           end
         end
 
-        context "with the wrong class" do
-          let(:expected_assign_class) { Class.new.tap {|klass| allow(klass).to receive(:name) { "BarClass" } } }
+        context "when not assigned" do
+          controller_action do
+            # no assign
+          end
 
           specify { expect(subject).to_not match(_controller) }
-          
+
           describe_failure_message do
-            specify { expect(subject).to eq "expected @assign to be assigned with an instance of #{expected_assign_class.name} but was assigned with an instance of #{actual_assign_class.name}"
-     }
+            specify { expect(subject).to eq "expected @assign to be assigned with an instance of #{expected_assign_class.name} but was not assigned" }
           end
         end
       end
 
       describe "#with_a_new" do
-        controller_action do
-          @assign = spec.actual_assign_class.new
-        end
-
-        let(:actual_persisted?) { false }
-        let(:actual_assign_class) do
-          Class.new.tap do |klass| 
-            allow(klass).to receive(:name) { "FooClass" }
-            allow_any_instance_of(klass).to receive(:persisted?) { actual_persisted? }
-          end
-        end
-
-        let(:expected_assign_class) { actual_assign_class }
+        let(:expected_assign_class) { Class.new.tap {|klass| allow(klass).to receive(:name) { "FooClass" } } }
 
         let(:matcher) { super().with_a_new(expected_assign_class) }
 
@@ -146,62 +164,88 @@ describe Warp::ControllerMatchers::AssignMatcher, type: :controller do
           specify { expect(subject).to eq "assign @assign with a new instance of #{expected_assign_class.name}" }
         end
 
-        context "with a new object" do
-          let(:actual_persisted?) { false }
+        context "when assigned" do
+          controller_action do
+            @assign = spec.actual_assign_class.new
+          end
 
-          context "with the right class" do
-            specify { expect(subject).to match(_controller) }
+          let(:actual_assign_class) { expected_assign_class }
 
-            describe_failure_message_when_negated do
-              specify { expect(subject).to eq "expected @assign to not be assigned with a new instance of #{expected_assign_class.name}" }
+          before do
+            actual_assign_class.tap do |klass|
+              allow_any_instance_of(klass).to receive(:persisted?) { actual_persisted? }
             end
           end
 
-          context "with a descendant class" do
-            let(:actual_assign_class) do
-              Class.new(super())
+          context "with a new object" do
+            let(:actual_persisted?) { false }
+
+            context "with the right class" do
+              specify { expect(subject).to match(_controller) }
+
+              describe_failure_message_when_negated do
+                specify { expect(subject).to eq "expected @assign to not be assigned with a new instance of #{expected_assign_class.name}" }
+              end
             end
 
-            specify { expect(subject).to match(_controller) }
+            context "with a descendant class" do
+              let(:actual_assign_class) do
+                Class.new(super())
+              end
 
-            describe_failure_message_when_negated do
-              specify { expect(subject).to eq "expected @assign to not be assigned with a new instance of #{expected_assign_class.name}" }
-            end 
+              specify { expect(subject).to match(_controller) }
+
+              describe_failure_message_when_negated do
+                specify { expect(subject).to eq "expected @assign to not be assigned with a new instance of #{expected_assign_class.name}" }
+              end
+            end
+
+            context "with the wrong class" do
+              let(:actual_assign_class) { Class.new.tap {|klass| allow(klass).to receive(:name) { "BarClass" } } }
+
+              specify { expect(subject).to_not match(_controller) }
+
+              describe_failure_message do
+                specify { expect(subject).to eq "expected @assign to be assigned with a new instance of #{expected_assign_class.name} but was assigned with a new instance of #{actual_assign_class.name}"
+         }
+              end
+            end
           end
 
-          context "with the wrong class" do
-            let(:expected_assign_class) { Class.new.tap {|klass| allow(klass).to receive(:name) { "BarClass" } } }
+          context "with a persisted object" do
+            let(:actual_persisted?) { true }
 
-            specify { expect(subject).to_not match(_controller) }
-            
-            describe_failure_message do
-              specify { expect(subject).to eq "expected @assign to be assigned with a new instance of #{expected_assign_class.name} but was assigned with a new instance of #{actual_assign_class.name}"
-       }
+            context "with the right class" do
+              specify { expect(subject).to_not match(_controller) }
+
+              describe_failure_message do
+                specify { expect(subject).to eq "expected @assign to be assigned with a new instance of #{expected_assign_class.name} but was assigned with a persisted instance of #{actual_assign_class.name}"
+         }
+              end
+            end
+
+            context "with the wrong class" do
+              let(:actual_assign_class) { Class.new.tap {|klass| allow(klass).to receive(:name) { "BarClass" } } }
+
+              specify { expect(subject).to_not match(_controller) }
+
+              describe_failure_message do
+                specify { expect(subject).to eq "expected @assign to be assigned with a new instance of #{expected_assign_class.name} but was assigned with a persisted instance of #{actual_assign_class.name}"
+         }
+              end
             end
           end
         end
 
-        context "with a persisted object" do
-          let(:actual_persisted?) { true }
-
-          context "with the right class" do
-            specify { expect(subject).to_not match(_controller) }
-            
-            describe_failure_message do
-              specify { expect(subject).to eq "expected @assign to be assigned with a new instance of #{expected_assign_class.name} but was assigned with a persisted instance of #{actual_assign_class.name}"
-       }
-            end
+        context "when not assigned" do
+          controller_action do
+            # no assign
           end
 
-          context "with the wrong class" do
-            let(:expected_assign_class) { Class.new.tap {|klass| allow(klass).to receive(:name) { "BarClass" } } }
+          specify { expect(subject).to_not match(_controller) }
 
-            specify { expect(subject).to_not match(_controller) }
-            
-            describe_failure_message do
-              specify { expect(subject).to eq "expected @assign to be assigned with a new instance of #{expected_assign_class.name} but was assigned with a persisted instance of #{actual_assign_class.name}"
-       }
-            end
+          describe_failure_message do
+            specify { expect(subject).to eq "expected @assign to be assigned with a new instance of #{expected_assign_class.name} but was not assigned" }
           end
         end
       end
@@ -225,7 +269,7 @@ describe Warp::ControllerMatchers::AssignMatcher, type: :controller do
           end
 
           behaviour do
-            specify { expect{ subject }.to raise_error("Only one of .with, .with_a, and .with_a_new can be used with the assigns matcher.") }
+            specify { expect{ subject }.to raise_error(NoMethodError) }
           end
         end
       end
